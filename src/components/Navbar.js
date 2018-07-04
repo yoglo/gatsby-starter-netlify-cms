4fafb86e9d5885801e580e421a23d3ed4fc2e7cc
Yoann Gloaguen <yo.gloaguen@gmail.com>
2018-07-04 10:29:07 +0200

    Index page modified for GHC. Navbar updated with responsive toggle button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link'
 
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
+import ghclogo from '../img/GlasgowHandballHeader.png'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -10,29 +11,26 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={ghclogo} alt="Kaldi" style={{ width: '65px' }} />
           </figure>
         </Link>
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          Abouut
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
+      <div class="navbar-menu">
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+          <Link className="navbar-item" to="/products">
+            Products
+          </Link>
+        </div>
       </div>
       <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import shirtimg from '../img/maillot2.jpg'
+import ghcmainlogo from '../img/GlasgowHandballLogo.png'
+import ballimg from '../img/ball.png'
+import trainingboardimg from '../img/training_board.png'
 
 export default class IndexPage extends React.Component {
   render() {
@@ -11,6 +15,48 @@ export default class IndexPage extends React.Component {
       <section className="section">
         <div className="container">
           <div className="content">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <figure className="image">
+                    <img src={ghcmainlogo} alt="GHC-logo" style={{ width: '330px' }} />
+                </figure>
+              </div>
+            </div>
+            <div
+                className="full-width-image-container margin-top-0"
+                style={{ backgroundImage: `url(${shirtimg})` }}
+              >
+            </div>
+            <div class="columns">
+              <div class="column is-7">
+                <h3 class="has-text-weight-semibold is-size-2">Join us</h3>
+                <p>
+                  Whether you're completely new or have many years of experience, play handball for Glasgow and come along to one of our free taster sessions! Everyone is friendly and welcoming, so why not give it a try?
+                </p>
+              </div>
+            </div>
+            <div class="columns is-multiline">
+              <div class="column is-6">
+                <section class="section">
+                  <p class="has-text-centered">
+                    <img alt="" src={ballimg}/>
+                  </p>
+                  <p>
+                    We currently have a senior Men and Ladies teams, but are always looking for new members! We welcome all ages (>16 years old) and abilities, and currently have players from many European countries with varying skill levels. We all have in common great interest in sport and want to contribute on and off the court in making Glasgow Handball Club great!
+                  </p>
+                </section>
+              </div>
+              <div class="column is-6">
+                <section class="section">
+                  <p class="has-text-centered">
+                    <img alt="" src={trainingboardimg}/>
+                  </p>
+                  <p>We offer a small, but carefully curated selection of brewing gear and tools for every taste and experience level. No matter if you roast your own beans or just bought your first french press, you’ll find a gadget to fall in love with in our shop.
+                  </p>
+                </section>
+              </div>
+            </div>
+            
             <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
           </div>
           {posts
