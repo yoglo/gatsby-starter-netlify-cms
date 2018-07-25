002a21c39d61bd99dacea1cbf1b31c578c60a234
Yoann Gloaguen <yo.gloaguen@gmail.com>
2018-07-25 19:45:23 +0200

    Remove "Products" from the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,9 +25,6 @@ const Navbar = () => (
           <Link className="navbar-item" to="/about">
             About
           </Link>
-          <Link className="navbar-item" to="/products">
-            Products
-          </Link>
         </div>
       </div>
       <div className="navbar-end">
