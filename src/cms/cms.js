a4c8ad04af5e4914fb82fa75d36c26c591f04a65
Yoann Gloaguen <yo.gloaguen@gmail.com>
2018-07-25 21:33:02 +0200

    Attempt adding new page with CMS support

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,8 +3,10 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import TeamPagePreview from './preview-templates/TeamPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('team', TeamPagePreview)
