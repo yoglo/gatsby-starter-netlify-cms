acd336d34f64c1d22ea906edf2246dbc3b15da6b
Yoann Gloaguen <yo.gloaguen@gmail.com>
2018-08-01 12:27:57 +0200

    Remove title team-page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Glasgow Handball club" />
     <Navbar />
     <div>{children()}</div>
   </div>
--- src/templates/team-page.js
+++ src/templates/team-page.js
@@ -25,17 +25,6 @@ export const TeamPageTemplate = ({
                 className="full-width-image-container margin-top-0"
                 style={{ backgroundImage: `url(${image})` }}
               >
-                <h2
-                  className="has-text-weight-bold is-size-1"
-                  style={{
-                    boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-                    backgroundColor: '#f40',
-                    color: 'white',
-                    padding: '1rem',
-                  }}
-                >
-                  {title}
-                </h2>
               </div>
               <div className="columns">
                 <div className="column is-7">
