ea529bbee6f021157df568e20c07201c5cb3a246
Yoann Gloaguen <yo.gloaguen@gmail.com>
2018-08-01 18:02:28 +0200

    cache back up

---
+++ .cache/async-requires.js
@@ -0,0 +1,37 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---src-templates-blog-post-js": require("gatsby-module-loader?name=component---src-templates-blog-post-js!/Users/yoanngloaguen/Documents/ghc_website/src/templates/blog-post.js"),
+  "component---src-templates-product-page-js": require("gatsby-module-loader?name=component---src-templates-product-page-js!/Users/yoanngloaguen/Documents/ghc_website/src/templates/product-page.js"),
+  "component---src-templates-about-page-js": require("gatsby-module-loader?name=component---src-templates-about-page-js!/Users/yoanngloaguen/Documents/ghc_website/src/templates/about-page.js"),
+  "component---src-templates-tags-js": require("gatsby-module-loader?name=component---src-templates-tags-js!/Users/yoanngloaguen/Documents/ghc_website/src/templates/tags.js"),
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/yoanngloaguen/Documents/ghc_website/.cache/dev-404-page.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/yoanngloaguen/Documents/ghc_website/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/yoanngloaguen/Documents/ghc_website/src/pages/index.js"),
+  "component---src-pages-tags-index-js": require("gatsby-module-loader?name=component---src-pages-tags-index-js!/Users/yoanngloaguen/Documents/ghc_website/src/pages/tags/index.js")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/layout-index.json"),
+  "blog-2016-12-17-making-sense-of-the-scaas-new-flavor-wheel.json": require("gatsby-module-loader?name=path---blog-2016-12-17-making-sense-of-the-scaas-new-flavor-wheel!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/blog-2016-12-17-making-sense-of-the-scaas-new-flavor-wheel.json"),
+  "blog-2017-01-04-a-beginners-guide-to-brewing-with-chemex.json": require("gatsby-module-loader?name=path---blog-2017-01-04-a-beginners-guide-to-brewing-with-chemex!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/blog-2017-01-04-a-beginners-guide-to-brewing-with-chemex.json"),
+  "blog-2017-01-04-just-in-small-batch-of-jamaican-blue-mountain-in-store-next-week.json": require("gatsby-module-loader?name=path---blog-2017-01-04-just-in-small-batch-of-jamaican-blue-mountain-in-store-next-week!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/blog-2017-01-04-just-in-small-batch-of-jamaican-blue-mountain-in-store-next-week.json"),
+  "products.json": require("gatsby-module-loader?name=path---products!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/products.json"),
+  "about.json": require("gatsby-module-loader?name=path---about!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/about.json"),
+  "tags-flavor.json": require("gatsby-module-loader?name=path---tags-flavor!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/tags-flavor.json"),
+  "tags-tasting.json": require("gatsby-module-loader?name=path---tags-tasting!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/tags-tasting.json"),
+  "tags-brewing.json": require("gatsby-module-loader?name=path---tags-brewing!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/tags-brewing.json"),
+  "tags-chemex.json": require("gatsby-module-loader?name=path---tags-chemex!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/tags-chemex.json"),
+  "tags-jamaica.json": require("gatsby-module-loader?name=path---tags-jamaica!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/tags-jamaica.json"),
+  "tags-green-beans.json": require("gatsby-module-loader?name=path---tags-green-beans!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/tags-green-beans.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/dev-404-page.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/404.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/index.json"),
+  "tags.json": require("gatsby-module-loader?name=path---tags!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/tags.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/yoanngloaguen/Documents/ghc_website/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "layout---index": require("gatsby-module-loader?name=component---src-layouts-index-js!/Users/yoanngloaguen/Documents/ghc_website/.cache/layouts/index.js")
+}
---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/yoanngloaguen/Documents/ghc_website/src/layouts/index.js"
+  import data from "/Users/yoanngloaguen/Documents/ghc_website/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
